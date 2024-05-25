8831c6d4e1f061055adb6c72923eecab523d8355
IanKilwiny <ribeiroian083@gmail.com>
2024-05-25 16:06:10 -0300

    A versão 2 atualizada

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # Sistema-Bancario-DIO
 Um Sistema Bancário feita na Plataforma DIO em Python
+
+* Novas informações atualizadas *
+- Modularização
+- Novas funções
+- Utilização de argumentos e retornos
+
+### Saques
+* argumentos - receber argumentos por nome (keyword only) *
+- saldo
+- valor
+- extrato
+- limite
+- num_saques
+- limite_saques
+
+* retorno *
+- saldo
+- extrato
+
+### Depósito
+* argumentos apenas por posicao (positional only)
+- saldo
+- valor
+- extrato
+
+* retorno *
+- saldo
+- extrato
+
+### Extrato
+* argmumentos por posicao e nome *
+- saldo (argumento posicional)
+- extrato (argumento nomeado)
+
+
+### Novas funções
+- criar usuário
+- criar conta corrente
+- listar_contas
+- listar_usuarios
+
+### Criar usuário
+- nome
+- data_nascimento
+- cpf
+- endereco (logradouro - bairro - cidade/sigladoestado)
+
+
+### Criar conta corrente
+- agencia (0001)
+- numero (vai adicionando 1 em 1)
+* obs: um usuário pode ter mais de uma conta *
+- usuario
+
+#Dica 
+- Filtrar uma lista de usuários
+
--- sistema_bancario_v1.py
+++ sistema_bancario_v1.py
@@ -1,65 +1,160 @@
+def mostrar_contas(lista_contas):
+    print("##########LISTA CONTAS##############")
+    for lista in lista_contas:
+        print(lista)
+    
+    print("###################################")
 
-saldo = 1000.00
-limite = 500.00
-numero_saques = 0
-numero_depositos = 0
-LIMITE_SAQUE = 3
-opcao = 0
-extrato = ''
+def mostrar_usuarios(lista_usarios):
+    print("###########LISTA USUARIOS############")
+    for lista in lista_usarios:
+        print(lista)
+    
+    print("####################################")
 
-def sacar(valor_saque):
-    global limite,saldo,numero_saques,extrato
+def verificar_usuario(lista_de_usario, cpf):
+    #filtro um usuário 
+    filtro = [usuario for usuario in lista_de_usario if usuario["cpf"] == cpf]
 
-    if valor_saque<=limite and numero_saques<LIMITE_SAQUE and saldo>valor_saque:
-        saldo  = saldo - valor_saque
-        numero_saques+=1
-        extrato +=f"Saque: {valor_saque:.2f}\n"
-    elif numero_saques == LIMITE_SAQUE:
-        print("Você passou o número de saques possíveis")
-    elif valor_saque > saldo:
-        print("Você não tem saldo suficiente")
-    elif valor_saque > limite:
-        print("O valor do saque passou do limite apropriado")
+    return filtro[0] if filtro else None 
+            
+def criar_usuario(nome, data_nasc, cpf, endereco):
 
-def depositar(valor):
-    global saldo, numero_depositos,extrato
+    dict_user = {
+        "nome":nome,
+        "nascimento":data_nasc,
+        "cpf":cpf,
+        "endereco":endereco
+    }
+    
+    return dict_user
+
+def criar_conta(*,cpf, num_conta, saldo):
+    AGENCIA = "0001"
+    num_conta += 1
+
+    conta = AGENCIA + str(num_conta)
+
+    dict_conta ={
+            "cpf":cpf,
+            "conta":conta,
+            "saldo":saldo,
+    }
+
+    return dict_conta, num_conta
+# keyword only - argumento por nome
+def sacar(saldo, valor, extrato, limite_valor ,num_saques, limite_num_saque):
+
+    if saldo>=valor and limite_valor>valor and num_saques<limite_num_saque:
+        saldo -=valor
+        extrato  += f'''Saque: R${valor: .2f} de R$ {saldo:.2f}\n'''
+        num_saques+=1  
+    elif saldo<valor:
+        print("Saldo Insuficiente")
+    elif valor > limite_valor:
+        print("Ultrapassou o limite de saque")
+    elif num_saques>limite_num_saque:
+        print("Ultrapassou o numero de saques permitido")
+
+    return saldo, extrato
+    
+
+#positional only - argumento por posicao
+def depositar(valor, saldo, extrato,/):
     if valor > 0:
         saldo+=valor
-        numero_depositos+=1
-        extrato+=f"Deposito: {valor:.2f}"
+        extrato+=f"Deposito: R$ {valor:.2f}\n"
     else:
         print("Deposito negativo")
 
-def mostrar_extrato():
-    return "Não tem movimentação" if not extrato else extrato 
-    
-    
+    return saldo,extrato
 
-while True:
-    print('''
-        [1] - Sacar
-        [2] - Depositar
-        [3] - Extrato
-        [4] - Sair
-    ''')
 
-    opcao = int(input("Escolha uma das opcões: "))
+def mostrar_estrato(saldo,/,*,extrato):
+    print("\n\n###########EXTRATO###########")
+            
+    print(extrato)
+    print(f"\n\nSaldo: {saldo:.2f}")
 
-    if(opcao == 1):
-        valor = float(input("Digite o valor do saque: "))
-        sacar(valor)
-    elif(opcao==2):
-        valor = float(input("Digite o valor de deposito: "))
-        depositar(valor)
-    elif(opcao==3):
-        print("\n\n###########EXTRATO###########")
+    print("################################")
+
+
+def main():
+    LIMITE_VALOR = 500.00
+    LIMITE_SAQUE = 3
+    opcao = 0
+    lista_usuario = []
+    lista_contas = []
+    saques= 0
+    extrato = ""
+    saldo = 1500.00
+    n_contas = 0
+
+    while True:
+        print('''
+            [1] - Sacar
+            [2] - Depositar
+            [3] - Extrato
+            [4] - Criar usuario
+            [5] - Criar conta
+            [6] - Listar usuarios
+            [7] - Listar contas
+            [8] - Sair
+        ''')
+
+        opcao = int(input("Escolha uma das opcões: "))
+
+        match opcao:
+            case 1:
+                valor = float(input("Digite o valor: "))
+                saldo,extrato= sacar(
+                    saldo=saldo,
+                    valor=valor,
+                    extrato=extrato,
+                    limite_valor=LIMITE_VALOR,
+                    limite_num_saque=LIMITE_SAQUE,
+                    num_saques=saques)
+
+            case 2:
+                valor = float(input("Digite o valor de deposito: "))
+
+                saldo, extrato = depositar(valor,saldo,extrato)
         
-        print(mostrar_extrato())
-        print(f"\n\nSaldo: {saldo:.2f}")
+            case 3:
+                mostrar_estrato(saldo,extrato=extrato)
+            case 4:
+                nome = input("Digite o seu nome completo: ")
+                nascimento = input("Digite sua data de nascimento (ex: xx/xx/xxxx): ")
+                cpf = input("Digite seu cpf (xxx.xxx.xxx-xx): ")
+                endereco  = input("Digite seu endereco (ex: logradouro - bairro - cidade/sigladoestado): ")
 
-        print("################################")
-    elif(opcao==4):
-        print("FIM DO PROGRAMA")
-        break
-    else:
-        print("O número digitado não está presente no menu")
+                if verificar_usuario(lista_usuario,cpf) == None:
+                    new_user =criar_usuario(nome,nascimento,cpf,endereco)
+                    lista_usuario.append(new_user)
+                else:
+                    print("Usuario existente")
+
+            case 5:
+
+                cpf = input("Digite seu cpf (xxx.xxx.xxx-xx): ")
+
+                if verificar_usuario(lista_usuario,cpf) != None:
+                    conta, n_contas = criar_conta(
+                        cpf=cpf,
+                        num_conta=n_contas,
+                        saldo = saldo
+                    )
+
+                    lista_contas.append(conta)
+                else:
+                    print("Usuário não existe")
+            case 6:
+                mostrar_usuarios(lista_usuario)
+            case 7:
+                mostrar_contas(lista_contas)
+            case 8:
+                break
+            case _:
+                print("O número digitado não está presente no menu")
+
+main()
